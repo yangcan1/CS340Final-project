b4ffc032821786d62d87d43e03260453ae81af98
Can Yang <yangca@flip3.engr.oregonstate.edu>
2023-06-02 23:35:51 -0700

    delete comment lines

--- app.js
+++ app.js
@@ -1,51 +1,3 @@
-// /*
-//     SETUP for a simple web app
-// */
-// // Express
-// var express = require('express');   // We are using the express library for the web server
-// var app     = express();            // We need to instantiate an express object to interact with the server in our code
-// PORT        = 20419;                 // Set a port number at the top so it's easy to change in the future
-// var db = require('./database/db_connector')
-
-// // Handlebars
-// const { engine } = require('express-handlebars');
-// var exphbs = require('express-handlebars');     // Import express-handlebars
-// app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
-// app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
-
-
-// /*
-//     ROUTES
-// */
-// app.get('/', function(req, res)
-//     {
-//         context = {};
-//         console.log("Rendering page");        
-//         console.log(context);
-//         // Render the users.handlebars file with the context
-     
-//         res.status(200).render('index', context);
-
-//     });
-
-// app.get('/Cars', function(req, res) 
-//     {
-//         let query1 = "SELECT * FROM Cars;";
-//         db.pool.query(query1, function(error, rows, fields) {
-//             res.render('Cars', {data: rows});
-//         })
-//     });
-
-// /*
-//     LISTENER
-// */
-// app.listen(PORT, function(){            // This is the basic syntax for what is called the 'listener' which receives incoming requests on the specified PORT.
-//     console.log('Express started on http://flip3.engr.oregonstate.edu:' + PORT + '; press Ctrl-C to terminate.')
-// });
-
-
-
-
 /*
     SETUP
 */
@@ -128,17 +80,6 @@ app.post('/addCar', function(req, res)
     let data = req.body;
 
     let id = parseInt(data.id);
-
-    //if (isNaN(id))
-    //{
-    //    id = 'NULL'
-    //}
-
-    //let year = parseInt(data.year);
-    //if (isNaN(year))
-    //{
-    //    year = 'NULL'
-    //}
 
     // Create the query and run it on the database
     query1 = `INSERT INTO Car (id, price, brand, model, year, color) VALUES (${data['id']}, '${data['price']}', '${data['brand']}', '${data['model']}', ${data['year']}, '${data['color']}')`;
