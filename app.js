6fc6b32a6fb6b476385ed7391271bfa18d002228
yangcan1 <yangca@flip3.engr.oregonstate.edu>
2023-06-06 14:33:00 -0700

    Insert funcs Completed

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'))
 
-PORT = 5080;
+PORT = 5050;
 
 // Database
 var db = require('./database/db-connector');
